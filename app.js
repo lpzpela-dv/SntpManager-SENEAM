01966c55545f4801c66024d6181b916c1917130d
pablolopezar <pabloe.lopezarmenta@outlook.com>
2022-04-04 00:36:08 -0700

    [Add] Estructurando codigo

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
-const mysql = require('mysql'); // librería de MySQL
 const snmpserv = require("net-snmp"); //Librería Agente SNMP
 const notify = require('./notifications-module'); // Modulo que genera notificaciones
 const snmp = require('./snmp-get'); // Modulo SNMP
 var conn = require('./mysql-module'); //Modulo BDD
 
 var session = snmpserv.createSession("192.168.7.68", "public"); // Se inicia sesión al Manager SNMP (Arduino)
-
-var con = mysql.createConnection({ host: 'localhost', user: 'root', password: 'seneam22' });
 
 //Se crea arreglo con las constantes de OIDs
 const toids = ["1.3.6.1.3.2016.1.0.1", "1.3.6.1.3.2016.2.0.1", "1.3.6.1.3.2016.3.0.1", "1.3.6.1.3.2016.4.0.1", "1.3.6.1.3.2016.5.0.1", "1.3.6.1.3.2016.6.0.1"
@@ -24,6 +21,8 @@ const toids = ["1.3.6.1.3.2016.1.0.1", "1.3.6.1.3.2016.2.0.1", "1.3.6.1.3.2016.3
 var losdatos = []; // Arreglo para almacenar respuestas de peticiones snmp
 var pet = 0; // Contador de peticiones para formateo de Query
 
+
+
 // Función que barre el arreglo de oids y obtiene los datos
 function getDataSnmpManager() {
     console.log("...Leyendo sensores...");
@@ -37,21 +36,10 @@ function getDataSnmpManager() {
 
 //Funcion que realiza la petición al arduino por SNMP, recibe como parametro el oid y la pos par almacenar en array losdatos[]
 function peticiones(oids, pos) {
-    // Declaración de objeto Date y formateo
-    // let date_ob = new Date();
-    // let date = ("0" + date_ob.getDate()).slice(-2);
-    // let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
-    // let year = date_ob.getFullYear();
-    // let hours = date_ob.getHours();
-    // let minutes = date_ob.getMinutes();
-    // let seconds = date_ob.getSeconds();
-    // var fuldate = year + "/" + month + "/" + date + " " + hours + ":" + minutes + ":" + seconds;
-
     // Petición SNMP al OID recibido como parametro en funcion Peticiones
     snmp.Get(session, oids, snmpserv).then((datos) => {
         pet += 1;
         losdatos[pos] = datos;
-        console.log(datos);
         if (pet == 58) {
             var cc = 0;
             var tmpquery = "";
@@ -67,8 +55,6 @@ function peticiones(oids, pos) {
                 }
                 cc += 1;
             }
-            console.log(tmpquery);
-
             //Se concatenan los valores obtenidos al query para almacenar en bdd
             tmpquery = "(now(), " + tmpquery + ")";
             tmpquery = "INSERT INTO monitoreo.energy_records (regtime, area_id,voltl1,ampl1,wattsl1,kwhl1,fpl1,hzl1,voltl2,ampl2,wattsl2,kwhl2,fpl2,hzl2,voltl3,ampl3,wattsl3,kwhl3,fpl3,hzl3,voltl4,ampl4,wattsl4,kwhl4,fpl4,hzl4,voltl5,ampl5,wattsl5,kwhl5,fpl5,hzl5,voltl6,ampl6,wattsl6,kwhl6,fpl6,hzl6,voltl7,ampl7,wattsl7,kwhl7,fpl7,hzl7,voltl8,ampl8,wattsl8,kwhl8,fpl8,hzl8,voltl9,ampl9,wattsl9,kwhl9,fpl9,hzl9, volDiesel, StCFE, StPlanta) values " + tmpquery;
@@ -86,22 +72,6 @@ function peticiones(oids, pos) {
                 });
                 console.log(alerts);
             });
-
-            //almacenando informaicón en BDD
-            // con.query(tmpquery, function (err, result, fields) {
-            //     if (err) throw err;
-            //     console.log("Guardado." + result.insertId);
-            //     //Validar lecturas para generar alarmas
-            //     let alerts = notify.validate(losdatos);
-            //     // Se mandan datos para generar las alarmas en caso de existir alguna
-            //     notify.generate(alerts).then(res => {
-            //         if (res != null) {
-            //             console.log(res)
-            //         }
-            //     });
-            //     console.log(alerts);
-            // });
-
 
         }
     }).catch((datos) => {
