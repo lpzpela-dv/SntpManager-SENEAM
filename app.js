a86dbd80e81358b9974c741b6760d341cc5e6c46
pablolopezar <pabloe.lopezarmenta@outlook.com>
2022-05-12 21:58:21 -0700

    [update]

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const toids = ["1.3.6.1.3.2016.1.0.1", "1.3.6.1.3.2016.2.0.1", "1.3.6.1.3.2016.3
     , "1.3.6.1.3.2016.1.0.7", "1.3.6.1.3.2016.2.0.7", "1.3.6.1.3.2016.3.0.7", "1.3.6.1.3.2016.4.0.7", "1.3.6.1.3.2016.5.0.7", "1.3.6.1.3.2016.6.0.7"
     , "1.3.6.1.3.2016.1.0.8", "1.3.6.1.3.2016.2.0.8", "1.3.6.1.3.2016.3.0.8", "1.3.6.1.3.2016.4.0.8", "1.3.6.1.3.2016.5.0.8", "1.3.6.1.3.2016.6.0.8"
     , "1.3.6.1.3.2016.1.0.9", "1.3.6.1.3.2016.2.0.9", "1.3.6.1.3.2016.3.0.9", "1.3.6.1.3.2016.4.0.9", "1.3.6.1.3.2016.5.0.9", "1.3.6.1.3.2016.6.0.9"
-    , "1.3.6.1.3.2016.7.0.2"
+    , "1.3.6.1.3.2016.7.0.2", "1.3.6.1.3.2016.7.0.3"
     , "1.3.6.1.3.2016.8.0.1", "1.3.6.1.3.2016.8.0.2"
 ];
 var losdatos = []; // Arreglo para almacenar respuestas de peticiones snmp
@@ -83,28 +83,30 @@ function peticiones(oids, pos) {
         // if (pet == 56) {
         //     calculaVolDiesel(losdatos[55]);
         // }
-        if (pet == 58) {
+        if (pet == 59) {
             var cc = 0;
             var tmpquery = "";
-            while (cc < 58) {
-                console.log(cc);
+            while (cc < 59) {
                 if (cc == 0) {
                     tmpquery = losdatos[cc];
                 }
                 else {
-                    if (losdatos[cc] < 0)
-                        tmpquery += ", " + 0;
-                    else {
-                        if (cc != 55)
-                            tmpquery += ", " + losdatos[cc] / 1000;
+                    if (cc != 56) {
+                        if (losdatos[cc] < 0)
+                            tmpquery += ", " + 0;
                         else {
-                            tmpquery += ", " + losdatos[cc];
-                            console.log(losdatos[cc]);
+                            if (cc != 55)
+                                tmpquery += ", " + losdatos[cc] / 1000;
+                            else {
+                                tmpquery += ", " + losdatos[cc];
+                                console.log(losdatos[cc]);
+                            }
                         }
                     }
                 }
                 cc += 1;
             }
+            console.log("Porcentaje de Diesel"+losdatos[56]);
             //Se concatenan los valores obtenidos al query para almacenar en bdd
             tmpquery = "(now(), " + tmpquery + ")";
             tmpquery = "INSERT INTO monitoreo.energy_records (regtime, area_id,voltl1,ampl1,wattsl1,kwhl1,fpl1,hzl1,voltl2,ampl2,wattsl2,kwhl2,fpl2,hzl2,voltl3,ampl3,wattsl3,kwhl3,fpl3,hzl3,voltl4,ampl4,wattsl4,kwhl4,fpl4,hzl4,voltl5,ampl5,wattsl5,kwhl5,fpl5,hzl5,voltl6,ampl6,wattsl6,kwhl6,fpl6,hzl6,voltl7,ampl7,wattsl7,kwhl7,fpl7,hzl7,voltl8,ampl8,wattsl8,kwhl8,fpl8,hzl8,voltl9,ampl9,wattsl9,kwhl9,fpl9,hzl9, volDiesel, StCFE, StPlanta) values " + tmpquery;
